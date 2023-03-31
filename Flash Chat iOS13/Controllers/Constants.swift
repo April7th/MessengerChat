aed7c0830379f9f9f02ccb70bbbe46ede67c6a4e
LeDuyTan <tanb1910699@student.ctu.edu.vn>
2023-03-31 21:07:42 +0700

    Initialzie project

---
+++ Flash Chat iOS13/Controllers/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Chat Project iOS13
+//
+//  Created by Lê Duy Tân on 31/03/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,10 +1,3 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
 
 import UIKit
 
@@ -14,7 +7,15 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var charIndex = 0.0
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
        
     }
     
